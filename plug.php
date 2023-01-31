85a37d6205ff2d7ff862073cb13522eac9718b60
Kariuki <mkmartinoes@gmail.com>
2023-01-31 17:27:23 +0300

    Initial commmit

---
+++ plug.php
@@ -0,0 +1,14 @@
+<?php
+/** 
+ * @package ThePlug
+ */
+
+ /* 
+  Plugin Name: Demo Plugin
+  Plugin URI: 'http://alecadd.com/plugin
+  Description: Custom plugin
+  Author: Martin *Emkay* Kariuki
+  Author URI: https://emkayint.com
+  License: GPLv2 or later
+  Text Domain: Demo-Plugin
+ */
