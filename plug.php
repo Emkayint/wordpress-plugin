b652dabc871b1f84abfd6d2867d1c18c94f9ba0c
Kariuki <mkmartinoes@gmail.com>
2023-01-31 18:11:01 +0300

    st: index.php file

--- plug.php
+++ plug.php
@@ -7,8 +7,9 @@
   Plugin Name: Demo Plugin
   Plugin URI: 'http://alecadd.com/plugin
   Description: Custom plugin
-  Author: Martin *Emkay* Kariuki
+  Author: Martin 
   Author URI: https://emkayint.com
   License: GPLv2 or later
   Text Domain: Demo-Plugin
+  Version: 1.0.0
  */
