c0d40a4d5990b5ea3c459de95760bd4132da8ef3
Kariuki <mkmartinoes@gmail.com>
2023-02-02 17:32:35 +0300

    ft: delete books on uninstall

--- unistall.php
+++ unistall.php
@@ -3,3 +3,9 @@
 if(! define("WP_UNISTALL_PLUGIN", true)){
  die;
 }
+
+$books = get_posts(array( "post_type" => "book", "numberposts" => -1));
+
+foreach( $books as $book){
+  wp_delete_post( $book -> ID, true);
+}
