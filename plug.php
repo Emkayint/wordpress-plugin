6aa95dac24efa8f7cd66afcbd8d8eab90eaed6a5
Kariuki <mkmartinoes@gmail.com>
2023-02-02 15:52:48 +0300

    ft: activation hook

--- plug.php
+++ plug.php
@@ -13,3 +13,57 @@
   Text Domain: Demo-Plugin
   Version: 1.0.0
  */
+
+//  if (! define( 'ABSPATH', true) ) {
+//   die;
+//  }
+
+ if( ! function_exists('add_action')) {
+  exit;
+ }
+
+ class SamplePlugin 
+ {
+    // Methods
+
+    function __construct(){
+      add_action('init', array($this, "custom_post_type"));
+    }
+   
+
+    function sayHello($arg){
+      echo  $arg; 
+    }
+
+    function activate() {
+      // echo "<p>Hello world</p>";
+      $this -> custom_post_type();
+      flush_rewrite_rules();
+    }
+
+
+    function deactivate() {
+
+    }
+
+    function uninstall() {
+
+    }
+
+    function custom_post_type () {
+      register_post_type('book', ['public' => true, "label" => "Book"]);
+    }
+ }
+
+ if (class_exists("SamplePlugin")) {
+  $init1 = new SamplePlugin();
+  // $init1 -> sayHello("Emkay");
+ }
+
+// activate plugin by calling class method activate
+ register_activation_hook(__FILE__, array($init1, "activate"));
+
+// deactivate plugin by calling class method deactivate
+
+ register_deactivation_hook(__FILE__, array($init1, "deactivate"));
+
