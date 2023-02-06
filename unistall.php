5516f298af24b98ef175dd919589acb12d9b30f5
Kariuki <mkmartinoes@gmail.com>
2023-02-06 13:09:48 +0300

    delete realtionships

--- unistall.php
+++ unistall.php
@@ -14,3 +14,6 @@ if(! define("WP_UNISTALL_PLUGIN", true)){
 
 global $wpdb;
 $wpdb -> query( "DELETE FROM wp_posts WHERE post_type = 'book'");
+$wpdb -> query( "DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT id FROM wp_posts)");
+
+$wpdb -> query("DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT id from wp_posts)");
